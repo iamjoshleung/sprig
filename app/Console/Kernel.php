b8f14814ea8a48086954a22f8b4d56f222a8d9a0
Joshua Leung <joshuajazleung@gmail.com>
2018-08-09 14:53:29 -0700

    update cron job interval

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->job(new ProcessTumblrImages)->everyThirtyMinutes();
+        $schedule->job(new ProcessTumblrImages)->everyFiveMinutes();
     }
 
     /**
