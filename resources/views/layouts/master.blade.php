16e0d6ae861f7c14a78d87f2a4462aba3a9a7b11
Joshua Leung <joshuajazleung@gmail.com>
2018-08-14 19:43:24 -0700

    add version number to css files (front end)

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -17,7 +17,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset(mix('css/app.css')) }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
