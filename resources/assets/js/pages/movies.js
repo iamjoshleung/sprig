8f13e11cc2d85ab9395dab36a494cc9d0ae3420c
Joshua Leung <joshuajazleung@gmail.com>
2018-09-28 19:16:10 -0700

    fix layout bugs

--- resources/assets/js/pages/movies.js
+++ resources/assets/js/pages/movies.js
@@ -2,7 +2,7 @@ import Masonry from "isotope-layout";
 
 if (document.querySelector(".js-page-movies-index")) {
   window.addEventListener("load", () => {
-    let msnry = new Masonry(".l-movies__items", {
+    let msnry = new Masonry(".js-items", {
       itemSelector: ".card",
       percentPosition: true,
       masonry: {
--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -4,18 +4,21 @@
     <div class="container">
         <div class="row l-movies__items">
             <div class="col-12">
-                @foreach($movies as $movie)
-                <a href="{{ route('movies.show', $movie) }}" class="card text-dark">
-                    <div class="card-img">
-                        {{ $movie->getMedia('cover')[0] }}
-                    </div>
+                <div class="js-items">
+                    @foreach($movies as $movie)
+                    <a href="{{ route('movies.show', $movie) }}" class="card text-dark d-block">
+                        <div class="card-img">
+                            {{ $movie->getMedia('cover')[0] }}
+                        </div>
 
-                    <div class="card-body">
-                        {{ $movie->title }}
-                    </div>
-                </a>
-                @endforeach
-
+                        <div class="card-body">
+                            {{ $movie->title }}
+                        </div>
+                    </a>
+                    @endforeach
+                </div>
+            </div>
+            <div class="col-12 d-flex justify-content-center mb-5">
                 {{ $movies->links() }}
             </div>
         </div>
